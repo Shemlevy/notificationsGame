054347a66f222d91e815cb17820cb0cad23371ab
shem <shemyafe@gmail.com>
2019-06-29 12:31:17 +0300

    feat:deploy test

---
+++ build/precache-manifest.02902376cdb0dbdcb606b360555e416f.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b2891166e570cb9a67ff5c29fd8484",
+    "url": "/notificationsGame/index.html"
+  },
+  {
+    "revision": "4b0ad3e8d6e1af54ff82",
+    "url": "/notificationsGame/static/css/main.f3cf1f97.chunk.css"
+  },
+  {
+    "revision": "c3e5f73c7b28749aa51b",
+    "url": "/notificationsGame/static/js/2.5a34c5cd.chunk.js"
+  },
+  {
+    "revision": "4b0ad3e8d6e1af54ff82",
+    "url": "/notificationsGame/static/js/main.0d1711b9.chunk.js"
+  },
+  {
+    "revision": "4148302a0af663f6904b",
+    "url": "/notificationsGame/static/js/runtime~main.b851f438.js"
+  },
+  {
+    "revision": "70727561347e707b032c8a4c2fdd6344",
+    "url": "/notificationsGame/static/media/barDown.70727561.mp3"
+  },
+  {
+    "revision": "5ba39ead650552338b503be8fd594ce2",
+    "url": "/notificationsGame/static/media/barUp.5ba39ead.mp3"
+  },
+  {
+    "revision": "575e4003ca9cb9270f2cc44698c317b9",
+    "url": "/notificationsGame/static/media/energy.575e4003.mp3"
+  },
+  {
+    "revision": "a65081d3498d9603777dfd972d46961c",
+    "url": "/notificationsGame/static/media/energyBad1.a65081d3.png"
+  },
+  {
+    "revision": "68d2bcea7d071d4ffd4b4c0580dfd7cb",
+    "url": "/notificationsGame/static/media/energyBad2.68d2bcea.png"
+  },
+  {
+    "revision": "6fd79a98d5ca31ebb9bd2833c90a35ee",
+    "url": "/notificationsGame/static/media/energyBad3.6fd79a98.png"
+  },
+  {
+    "revision": "36db165b18b2dee8fc8d2f09c174be72",
+    "url": "/notificationsGame/static/media/energyBad4.36db165b.png"
+  },
+  {
+    "revision": "e33f18729f34590ea44024d99ce62e8f",
+    "url": "/notificationsGame/static/media/energyBad5.e33f1872.png"
+  },
+  {
+    "revision": "f649b2c78f95ea67538c463269e4546a",
+    "url": "/notificationsGame/static/media/energyBad6.f649b2c7.png"
+  },
+  {
+    "revision": "1a71a0dd62699b833bc3a2182683287c",
+    "url": "/notificationsGame/static/media/energyGood1.1a71a0dd.png"
+  },
+  {
+    "revision": "006b9b8d3f1a4ed61de2bb87ea62d1e3",
+    "url": "/notificationsGame/static/media/energyGood2.006b9b8d.png"
+  },
+  {
+    "revision": "7b34b6f20adfe2d5abfa101fd4a219cf",
+    "url": "/notificationsGame/static/media/energyGood3.7b34b6f2.png"
+  },
+  {
+    "revision": "52a67b8dde55544e2bf660e46aafd117",
+    "url": "/notificationsGame/static/media/energyGood4.52a67b8d.png"
+  },
+  {
+    "revision": "a636b469248f1b1e2da65bb9ec7c1d86",
+    "url": "/notificationsGame/static/media/energyGood5.a636b469.png"
+  },
+  {
+    "revision": "ba9d7d9f12c1335acd4d4741a8238a6c",
+    "url": "/notificationsGame/static/media/energyGood6.ba9d7d9f.png"
+  },
+  {
+    "revision": "7bab34935a2a9a588f0b6f0fe8dfd6be",
+    "url": "/notificationsGame/static/media/esteem.7bab3493.mp3"
+  },
+  {
+    "revision": "9a1cc16cc181fcf12fa39e5f53864ba1",
+    "url": "/notificationsGame/static/media/esteemBad1.9a1cc16c.png"
+  },
+  {
+    "revision": "0350f6c8775b1c88584377d2f26eeb1a",
+    "url": "/notificationsGame/static/media/esteemBad2.0350f6c8.png"
+  },
+  {
+    "revision": "41aae6bfa73853091ffd89725eb7447e",
+    "url": "/notificationsGame/static/media/esteemBad3.41aae6bf.png"
+  },
+  {
+    "revision": "6b4d2ddb066a058f97d63a0acd44e669",
+    "url": "/notificationsGame/static/media/esteemBad4.6b4d2ddb.png"
+  },
+  {
+    "revision": "908218e02bf369b25ef8a301707473d2",
+    "url": "/notificationsGame/static/media/esteemBad5.908218e0.png"
+  },
+  {
+    "revision": "8cf258e7efa2230c6d93bb8fa7b36918",
+    "url": "/notificationsGame/static/media/esteemBad6.8cf258e7.png"
+  },
+  {
+    "revision": "bc7a05c312c6294feb89a491f5af5a9d",
+    "url": "/notificationsGame/static/media/esteemGood1.bc7a05c3.png"
+  },
+  {
+    "revision": "2c15980043fd6d75196427c46a57a4c7",
+    "url": "/notificationsGame/static/media/esteemGood2.2c159800.png"
+  },
+  {
+    "revision": "19441379f0cc474a77cd8d73f5304af2",
+    "url": "/notificationsGame/static/media/esteemGood3.19441379.png"
+  },
+  {
+    "revision": "8d17e78ea4466e44515f4cc68c63673f",
+    "url": "/notificationsGame/static/media/esteemGood4.8d17e78e.png"
+  },
+  {
+    "revision": "811ff993b5789952d71f0cd3fe06d65f",
+    "url": "/notificationsGame/static/media/esteemGood5.811ff993.png"
+  },
+  {
+    "revision": "f005d06b7e6b310975d184cd40b6c6d0",
+    "url": "/notificationsGame/static/media/esteemGood6.f005d06b.png"
+  },
+  {
+    "revision": "7b1a22f150f4379c35c415217780bcdd",
+    "url": "/notificationsGame/static/media/growth.7b1a22f1.mp3"
+  },
+  {
+    "revision": "10051f12eec0ce759f4500c3386c8f64",
+    "url": "/notificationsGame/static/media/growthBad1.10051f12.png"
+  },
+  {
+    "revision": "54f56cfc2374e3a17451948fe1acbabc",
+    "url": "/notificationsGame/static/media/growthBad2.54f56cfc.png"
+  },
+  {
+    "revision": "30a83cfb70b5862d883ac2e8a3446498",
+    "url": "/notificationsGame/static/media/growthBad3.30a83cfb.png"
+  },
+  {
+    "revision": "d84cde5c8534bfa7bc04455628a53b06",
+    "url": "/notificationsGame/static/media/growthBad4.d84cde5c.png"
+  },
+  {
+    "revision": "5623d3ac3298e7657738fbfd648e0813",
+    "url": "/notificationsGame/static/media/growthBad5.5623d3ac.png"
+  },
+  {
+    "revision": "adeb18de2526781242d7399cf2a4aabf",
+    "url": "/notificationsGame/static/media/growthBad6.adeb18de.png"
+  },
+  {
+    "revision": "9bc971a9280a905d4ed954b76e41c35e",
+    "url": "/notificationsGame/static/media/growthGood1.9bc971a9.png"
+  },
+  {
+    "revision": "c580e549ba984b31c42cdec33c11047a",
+    "url": "/notificationsGame/static/media/growthGood2.c580e549.png"
+  },
+  {
+    "revision": "643df479556db271b3de7e81390f1e76",
+    "url": "/notificationsGame/static/media/growthGood3.643df479.png"
+  },
+  {
+    "revision": "a60f87b097e7d363a0f2beecee6b072f",
+    "url": "/notificationsGame/static/media/growthGood4.a60f87b0.png"
+  },
+  {
+    "revision": "783c3bc5f4ec88689cc1a8f553d206a5",
+    "url": "/notificationsGame/static/media/growthGood5.783c3bc5.png"
+  },
+  {
+    "revision": "e33e7a36720bd2638061a66abbeeb012",
+    "url": "/notificationsGame/static/media/growthGood6.e33e7a36.png"
+  },
+  {
+    "revision": "fe2aac0ad9e8cfd598270a72e944ec5b",
+    "url": "/notificationsGame/static/media/love.fe2aac0a.mp3"
+  },
+  {
+    "revision": "cdfed7364f609a72fb678c1f6af8aab9",
+    "url": "/notificationsGame/static/media/loveBad1.cdfed736.png"
+  },
+  {
+    "revision": "34f1574f671177021128b4a6e2421fbb",
+    "url": "/notificationsGame/static/media/loveBad2.34f1574f.png"
+  },
+  {
+    "revision": "95c96e8cf9a6f0024a0642da3fb2c1b4",
+    "url": "/notificationsGame/static/media/loveBad3.95c96e8c.png"
+  },
+  {
+    "revision": "914373fa704957d8de1521050d9b16b5",
+    "url": "/notificationsGame/static/media/loveBad4.914373fa.png"
+  },
+  {
+    "revision": "7606d56d1d4a1500e06a4fc69e4c1aa8",
+    "url": "/notificationsGame/static/media/loveBad5.7606d56d.png"
+  },
+  {
+    "revision": "d766fc64534a52ba574c0902abca2511",
+    "url": "/notificationsGame/static/media/loveBad6.d766fc64.png"
+  },
+  {
+    "revision": "2ff2afa55c508f4da588898e0b0f5ac0",
+    "url": "/notificationsGame/static/media/loveGood1.2ff2afa5.png"
+  },
+  {
+    "revision": "549d3ef45f40de879a220a1d0b19de24",
+    "url": "/notificationsGame/static/media/loveGood2.549d3ef4.png"
+  },
+  {
+    "revision": "de1d0607da7801c562c1d97bc409b1d0",
+    "url": "/notificationsGame/static/media/loveGood3.de1d0607.png"
+  },
+  {
+    "revision": "56e8bd15ca1c4babc02430b660fed26e",
+    "url": "/notificationsGame/static/media/loveGood4.56e8bd15.png"
+  },
+  {
+    "revision": "79c69d98fa8ede0149bedecf49602aa5",
+    "url": "/notificationsGame/static/media/loveGood5.79c69d98.png"
+  },
+  {
+    "revision": "5a2b69e3cc18a51e3c76750e2fd57a65",
+    "url": "/notificationsGame/static/media/loveGood6.5a2b69e3.png"
+  },
+  {
+    "revision": "32a3a1a03a6d2aaa8e91803e2191746c",
+    "url": "/notificationsGame/static/media/safety.32a3a1a0.mp3"
+  },
+  {
+    "revision": "3bb85f3498e9d70fa7cbf29b9c4c056e",
+    "url": "/notificationsGame/static/media/safetyBad1.3bb85f34.png"
+  },
+  {
+    "revision": "70cff35c188aef6f6878f212beb09340",
+    "url": "/notificationsGame/static/media/safetyBad2.70cff35c.png"
+  },
+  {
+    "revision": "7382f9cfb1ed26b9d7d7bb8188bf689b",
+    "url": "/notificationsGame/static/media/safetyBad3.7382f9cf.png"
+  },
+  {
+    "revision": "91b431e16a838c7cf707b3914aba6176",
+    "url": "/notificationsGame/static/media/safetyBad4.91b431e1.png"
+  },
+  {
+    "revision": "789ca9c37e9f05b848b9164462f6c920",
+    "url": "/notificationsGame/static/media/safetyBad5.789ca9c3.png"
+  },
+  {
+    "revision": "c4573f7100ab61964eb8e4475b7345f2",
+    "url": "/notificationsGame/static/media/safetyBad6.c4573f71.png"
+  },
+  {
+    "revision": "a150e8003e28eb621cb86361eff85481",
+    "url": "/notificationsGame/static/media/safetyGood1.a150e800.png"
+  },
+  {
+    "revision": "e48c3e3e8f4f91217019a69734d8dc0d",
+    "url": "/notificationsGame/static/media/safetyGood2.e48c3e3e.png"
+  },
+  {
+    "revision": "52cad8d0b432c87e17d91abeb4eea4af",
+    "url": "/notificationsGame/static/media/safetyGood3.52cad8d0.png"
+  },
+  {
+    "revision": "35f5bedece3e15d174bc3b356f585272",
+    "url": "/notificationsGame/static/media/safetyGood4.35f5bede.png"
+  },
+  {
+    "revision": "f0764ac3d17e65251fe87e80f89f4808",
+    "url": "/notificationsGame/static/media/safetyGood5.f0764ac3.png"
+  },
+  {
+    "revision": "1e0ccab121b2d71515cefe5dd77dee2f",
+    "url": "/notificationsGame/static/media/safetyGood6.1e0ccab1.png"
+  },
+  {
+    "revision": "5a992e442a8130886970915d357ee477",
+    "url": "/notificationsGame/static/media/trashSound.5a992e44.mp3"
+  }
+]);
